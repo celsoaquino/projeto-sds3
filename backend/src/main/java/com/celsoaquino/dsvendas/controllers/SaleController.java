e4e95d75ebdd336ffc8ad2db6f907d68d9c2c717
celso <celso.aquino@ymail.com>
2021-05-09 13:03:07 -0300

    Sales endpoint adjustments

--- backend/src/main/java/com/celsoaquino/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/celsoaquino/dsvendas/controllers/SaleController.java
@@ -9,19 +9,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/sales")
 public class SaleController {
 
     @Autowired
     private SaleService saleService;
 
-    @GetMapping
+    @GetMapping("/sales")
     public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
         Page<SaleDTO> list = saleService.findAll(pageable);
         return ResponseEntity.ok(list);
